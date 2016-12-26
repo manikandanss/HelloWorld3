7c29172718a41d5f56875e8e4444eae479012175
manikandanshan <manikbhuvan1985@gmail.com>
2016-12-26 19:02:01 +0530

    committed succesfulyy

--- Simple.java
+++ Simple.java
@@ -3,6 +3,7 @@ class Simple{
      System.out.println("Hello Java"); 
      System.out.println("Hello Jenkins"); 
      System.out.println("Hello1 "); 
-     System.out.println("HDFS");     
+     System.out.println("HDFS");   
+  System.out.println(rfgjkdfjkjkfg");  
     }  
 }
