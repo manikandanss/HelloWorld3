3e245d673652b3b87d72666ae2653512f617e3c2
manikandanshan <manikbhuvan1985@gmail.com>
2018-05-14 16:25:13 +0530

    committed

--- Simple.java
+++ Simple.java
@@ -1,9 +1,9 @@
 public class Simple{  
     public static void main(String args[]){  
-     System.out.println(Hello Java"); 
+     System.out.println("Hello Java"); 
      System.out.println("Hello Jenkins"); 
      System.out.println("Hello world "); 
-     System.out.println(HDFS");   
+     System.out.println("HDFS");   
   System.out.println("rfgjkdfjkjkfg");  
     }  
 }
