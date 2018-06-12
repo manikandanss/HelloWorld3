a8dce8f38f38963de81679aa6fe6c6124a446b2e
manikandanss <manikandan.ss@guruits.com>
2018-06-12 15:03:26 +0530

    Updated yugan printf statement

--- Simple.java
+++ Simple.java
@@ -2,8 +2,8 @@ public class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java"); 
      System.out.println("Hello Jenkins"); 
-     System.out.println("Hello world "); 
-     System.out.println("HDFS");   
+     System.out.println("Hello Yugan Welcome"); 
+     
   System.out.println("rfgjkdfjkjkfg");  
     }  
 }
