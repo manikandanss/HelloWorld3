c4e85f122a325108e75e684571b467838c3b8668
manikandanss <manikandan.ss@guruits.com>
2019-05-30 12:49:24 +0530

    resolved code issues in line 2,3 ,4 in Simple.Java

--- Simple.java
+++ Simple.java
@@ -1,7 +1,7 @@
 public class Simple{  
-     static void main(String args[]){  
-     system.out.println("Hello Java"); 
-     system.out.println("Hello Jenkins"); 
+     public static void main(String args[]){  
+     System.out.println("Hello Java"); 
+     System.out.println("Hello Jenkins"); 
      System.out.println("Hello world "); 
      System.out.println("HDFS");   
   System.out.println("HI@@XXXXXXXX");  
